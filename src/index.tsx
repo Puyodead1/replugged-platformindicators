0eee2b2134a18c93441b962bdcf7db81be4a36d1
Puyodead1 <puyodead@proton.me>
2023-06-13 18:35:01 -0400

    use renderInChat setting

--- src/index.tsx
+++ src/index.tsx
@@ -124,6 +124,7 @@ export async function start(): Promise<void> {
   if (!messageHeaderFnName) return logger.error("Failed to get message header function name");
 
   inject.after(messageHeaderModule, messageHeaderFnName, ([args], res, _) => {
+    if (!cfg.get("renderInChat")) return undefined;
     const user = args.message.author as User;
     if (args.decorations && args.decorations["1"] && args.message && user) {
       const a = (
