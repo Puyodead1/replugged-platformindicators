df9a87e2ef633e56c54cb9fead626e352b6c95ef
Puyodead1 <puyodead@proton.me>
2023-06-12 23:49:43 -0400

    remove debug log

--- src/index.tsx
+++ src/index.tsx
@@ -124,8 +124,6 @@ export async function start(): Promise<void> {
   if (!messageHeaderFnName) return logger.error("Failed to get message header function name");
 
   inject.after(messageHeaderModule, messageHeaderFnName, ([args], res, _) => {
-    console.log(cfg.get("renderInChat"));
-
     const user = args.message.author as User;
     if (args.decorations && args.decorations["1"] && args.message && user) {
       const a = (
